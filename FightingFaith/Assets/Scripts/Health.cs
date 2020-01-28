424bc639168c0c940e47683c2530206ae4c913c0
moradavi <57117336+moradavi@users.noreply.github.com>
2020-01-28 17:17:05 -0500

    Create Health Script
    Create Health.cs and declare basic functions and variables. Functions still need to be implemented.

---
+++ FightingFaith/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    int health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ResetHealth()
+    {
+
+    }
+
+    void LoseHealth()
+    {
+
+    }
+
+    void ReplenishHealth()
+    {
+
+    }
+}
