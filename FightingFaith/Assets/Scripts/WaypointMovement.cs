5a4e8d1978339fcad2d673bafe73a2728d4e2788
moradavi <57117336+moradavi@users.noreply.github.com>
2020-01-29 16:49:09 -0500

    Create Waypoint Movement Script
    Create waypoint movement branch, has no functionality yet.

---
+++ FightingFaith/Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMovement : MonoBehaviour
+{
+
+    public List<Transform> waypoints = new List<Transform>();
+    public float waypointPauseTime;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveTo(Transform waypoint)
+    {
+
+    }
+
+}
