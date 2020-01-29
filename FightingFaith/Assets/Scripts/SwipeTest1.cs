e4b7fda56fa9f3e601723f2d7661486b80156da0
arrozm <57187091+arrozm@users.noreply.github.com>
2020-01-29 17:45:22 -0500

    Start Swipe Mechanic 1
    Swipe Mechanic 1 functionality added.

---
+++ FightingFaith/Assets/Scripts/SwipeTest1.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTest1 : MonoBehaviour
+{
+    public bool isSwiping;
+
+    Rigidbody2D rb;
+    Camera cam;
+    CircleCollider2D circleColl;
+
+    Vector2 oldPos;
+    public float minSlashSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+        circleColl = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartSwiping();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            EndSwiping();
+        }
+
+        if (isSwiping)
+        {
+            UpdateSwipe();
+        }
+    }
+
+    void UpdateSwipe()
+    {
+        Vector2 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        rb.position = newPos;
+        float velocity = (newPos - oldPos).magnitude * Time.deltaTime;
+
+        if (velocity > minSlashSpeed)
+        {
+            circleColl.enabled = true;
+        } else
+        {
+            circleColl.enabled = false;
+        }
+
+        oldPos = newPos;
+    }
+
+    void StartSwiping()
+    {
+        isSwiping = true;
+        oldPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        circleColl.enabled = false;
+    }
+    void EndSwiping()
+    {
+        isSwiping = false;
+        circleColl.enabled = false;
+    }
+}
