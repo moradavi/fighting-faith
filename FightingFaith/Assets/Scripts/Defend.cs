87c3e3414f683d5a074c034262ac28f0b18f1e79
xinranmadesign <59710058+xinranmadesign@users.noreply.github.com>
2020-01-29 18:17:50 -0500

    Setup Defend Scene
    Setup the defend scene, points, and colliders.

--- FightingFaith/Assets/Scripts/Defend.cs
+++ FightingFaith/Assets/Scripts/Defend.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Defend : MonoBehaviour
 {
+
+    public GameObject[] defensePoints;
+    public int numOfPoints;
+
+    public float timeLimit;
+    float timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer >= timeLimit)
+        {
+            Debug.Log("GAME OVER");
+        }
+
     }
 }
