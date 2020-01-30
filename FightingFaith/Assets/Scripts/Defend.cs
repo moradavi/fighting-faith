caf3f6014870c3454134c333ed2514b156fb0221
xinranmadesign <59710058+xinranmadesign@users.noreply.github.com>
2020-01-30 16:19:39 -0500

    Generate Random Defense Pattern
    Defense script can generate patterns.

--- FightingFaith/Assets/Scripts/Defend.cs
+++ FightingFaith/Assets/Scripts/Defend.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Defend : MonoBehaviour
 {
-
-    public GameObject[] defensePoints;
+    
     public int numOfPoints;
 
     public float timeLimit;
     float timer;
 
+    public List<GameObject> allPoints;
+
+    public List<GameObject> defensePoints;
+    public List<GameObject> pointsToHit;
+
     // Start is called before the first frame update
     void Start()
     {
         timer = 0;
+        
+        defensePoints = allPoints;
+        
+        for( int i = 0; i < numOfPoints; i++)
+        {
+            int indexPoint = Random.Range(0, defensePoints.Count - 1);
+            GameObject randomPoint = defensePoints[indexPoint];
+
+            pointsToHit.Add(randomPoint);
+            defensePoints.RemoveAt(indexPoint);
+        }
+        
     }
 
     // Update is called once per frame
@@ -26,6 +42,13 @@ public class Defend : MonoBehaviour
         {
             Debug.Log("GAME OVER");
         }
+
+
+        //if (Input.GetMouseButton(0))
+        //{
+
+        //}
+
 
     }
 }
