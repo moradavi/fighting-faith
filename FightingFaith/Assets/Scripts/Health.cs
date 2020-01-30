678a93f336cfc534fabb7272fcb67e9edea42b24
moradavi <57117336+moradavi@users.noreply.github.com>
2020-01-30 12:23:08 -0500

    Update Health.cs Functionality
    Renamed ReplenishHealth() to GainHealth() for conciseness. Refactored health gain and loss to be more clear and efficient. Added death property and event. Added EmptyHealth() to instantly kill. Enemy-Framework scene now has health example with UI buttons.

--- FightingFaith/Assets/Scripts/Health.cs
+++ FightingFaith/Assets/Scripts/Health.cs
@@ -1,31 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int maxHealth;
-    int health;
-    
+    public bool IsDead { get; private set; }
+    public int CurrentHealth { get; private set; }
+    public UnityEvent onDeath = new UnityEvent();
+
     // Start is called before the first frame update
     void Start()
     {
         ResetHealth();
     }
 
-    void ResetHealth()
+    public void LoseHealth(int damage)
     {
-        health = maxHealth;
+        if(damage >= 0)
+            ChangeHealthBy(-damage);
+        else
+            Debug.LogWarning("Damage values must be positive");
     }
 
-    void LoseHealth(int damage)
+    public void GainHealth(int heal)
     {
-        health -= damage;
+        if (heal >= 0)
+            ChangeHealthBy(heal);
+        else
+            Debug.LogWarning("Heal values must be positive");
     }
 
-    void ReplenishHealth(int heal)
+    public void ResetHealth()
     {
-        health += heal;
-        health = Mathf.Clamp(health, 0, maxHealth);
+        CurrentHealth = maxHealth;
+        IsDead = false;
+    }
+
+    public void EmptyHealth()
+    {
+        CurrentHealth = 0;
+        Die();
+    }
+
+    void ChangeHealthBy(int i)
+    {
+        CurrentHealth += i;
+        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, maxHealth);
+
+        if (CurrentHealth <= 0)
+            Die();
+        else
+            IsDead = false;
+    }
+
+    void Die()
+    {
+        onDeath.Invoke();
+        IsDead = true;
     }
 }
