36fac4c2451383875fd3e16b4975085dd38b8b98
moradavi <57117336+moradavi@users.noreply.github.com>
2020-01-30 12:28:20 -0500

    Update Collider2D Reference in Enemy.cs
    Renamed Collider2D reference from "collider" to "enemyCollider" in Enemy.cs to not hide inherented Component.collider. Made reference public, and updated Enemy.prefab to fill the Collider2D reference in the inspector.

--- FightingFaith/Assets/Scripts/Enemy.cs
+++ FightingFaith/Assets/Scripts/Enemy.cs
@@ -6,7 +6,7 @@ public class Enemy : MonoBehaviour
 {
     public Health health;
     public WaypointMovement waypointMovement;
-    Collider2D collider;
+    public Collider2D enemyCollider;
     
     // Start is called before the first frame update
     void Start()
