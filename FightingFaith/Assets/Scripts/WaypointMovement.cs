b23ddc50b483f2a6bcc40120d27e0c3e53478c9f
moradavi <57117336+moradavi@users.noreply.github.com>
2020-01-30 11:48:56 -0500

    Add Waypoint Movement Functionality
    WaypointMovement.cs is now functional. The script moves object to random waypoints in Vector2 space at the provided speed, then pauses for provided amount of time before moving to next random waypoint. Example can be seen in Enemy-Framework.unity.

--- FightingFaith/Assets/Scripts/WaypointMovement.cs
+++ FightingFaith/Assets/Scripts/WaypointMovement.cs
@@ -1,29 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WaypointMovement : MonoBehaviour
-{
-
+{  
     public List<Transform> waypoints = new List<Transform>();
     public float waypointPauseTime;
     public float speed;
+
+    Transform targetWaypoint;
+    Transform currentWaypoint;
+    int targetWaypointIndex;
     
+    public bool IsMoving { get; private set; }
+    public UnityEvent onTargetArrive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set intial target waypoint
+        targetWaypointIndex = Random.Range(0, waypoints.Count);
+        SetTargetWaypoint(waypoints[targetWaypointIndex]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move towards the target waypoint
+        if (IsMoving)
+            transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, Time.deltaTime * speed);
+
+        //If arrived at the target waypoint
+        if ((transform.position == targetWaypoint.position) && IsMoving == true)
+        {
+            //Pause for set amount of time, then move to next waypoint
+            onTargetArrive.Invoke();
+            currentWaypoint = targetWaypoint;
+            IsMoving = false;
+            Invoke("SetNewRandomTargetWaypoint", waypointPauseTime);
+        }
     }
 
-    void MoveTo(Transform waypoint)
+    public void StopWaypointMovement()
     {
+        IsMoving = false;
+        CancelInvoke();
+    }
 
+    public void ResumeWaypointMovement()
+    {
+        IsMoving = true;
+    }
+
+    void SetNewRandomTargetWaypoint()
+    {
+        targetWaypointIndex = RandomExcept(0, waypoints.Count, targetWaypointIndex);
+        SetTargetWaypoint(waypoints[targetWaypointIndex]);
+    }
+
+    void SetTargetWaypoint(Transform waypoint)
+    {
+        targetWaypoint = waypoint;
+        currentWaypoint = null;
+        IsMoving = true;
+    }
+
+    public int RandomExcept(int min, int max, int except)
+    {
+        int random = Random.Range(min, max);
+        if (random >= except) random = (random + 1) % max;
+        return random;
     }
 
 }
