280ef97eec45f2c142b4fbf21082d9e72e012a43
moradavi <57117336+moradavi@users.noreply.github.com>
2020-04-02 19:40:13 -0400

    Added Lerp Movement and Adjusted Attack Animation

--- FightingFaith/Assets/Scripts/WaypointMovement.cs
+++ FightingFaith/Assets/Scripts/WaypointMovement.cs
@@ -9,15 +9,24 @@ public class WaypointMovement : MonoBehaviour
     public float waypointPauseTime;
     public float speed;
 
+    //Float Lerp Values
+    float lerpTime;
+    float currentLerpTime;
+    float currentLerpTimer;
+    float currentlerpDistance;
+
     //Waypoint Data
     public List<Transform> waypoints = new List<Transform>();
+    public int startWaypointIndex;
     Transform targetWaypoint;
     Transform currentWaypoint;
+    Transform homeWaypoint;
     int targetWaypointIndex;
 
     //Bool Properties
     public bool IsMoving { get; private set; }
     public bool IsStopped { get; private set; }
+    bool hasArrived;
 
     //Events
     public UnityEvent onTargetArrive;
@@ -28,8 +37,10 @@ public class WaypointMovement : MonoBehaviour
     {
         //Set intial target waypoint
         IsStopped = false;
-        targetWaypointIndex = Random.Range(0, waypoints.Count);
-        SetTargetWaypoint(waypoints[targetWaypointIndex]);
+        homeWaypoint = waypoints[startWaypointIndex];
+        transform.position = homeWaypoint.position;
+        targetWaypointIndex = startWaypointIndex;
+        SetNewRandomTargetWaypoint();
     }
 
     // Update is called once per frame
@@ -37,7 +48,11 @@ public class WaypointMovement : MonoBehaviour
     {
         //Move towards the target waypoint
         if (IsMoving)
-            transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, Time.deltaTime * speed);
+        {
+            //transform.position = Vector2.MoveTowards(transform.position, targetWaypoint.position, Time.deltaTime * speed);
+            LerpTowardsTarget();
+        }
+            
 
         //If arrived at the target waypoint
         if ((transform.position == targetWaypoint.position) && IsMoving == true)
@@ -45,9 +60,35 @@ public class WaypointMovement : MonoBehaviour
             //Pause for set amount of time, then move to next waypoint
             onTargetArrive.Invoke();
             currentWaypoint = targetWaypoint;
+            homeWaypoint = targetWaypoint;
             IsMoving = false;
             Invoke("SetNewRandomTargetWaypoint", waypointPauseTime);
         }
+    }
+
+    private void LerpTowardsTarget()
+    {
+        currentLerpTimer += Time.deltaTime;           
+        if(currentLerpTimer > currentLerpTime)
+        {
+            currentLerpTimer = currentLerpTime;
+        }
+ 
+        float perc = currentLerpTimer / currentLerpTime;
+        perc = perc * perc * (3f - 2f * perc);
+        transform.position = Vector2.Lerp(homeWaypoint.position, targetWaypoint.position, perc);
+
+    }
+
+    private void SetLerpValues(Vector3 start, Vector3 target)
+    {
+        currentlerpDistance = Vector2.Distance(homeWaypoint.position, targetWaypoint.position);
+        currentLerpTime = currentlerpDistance / speed;
+    }
+
+    private void ResetLerpValues()
+    {
+        currentLerpTimer = 0;
     }
 
     public void StopWaypointMovement()
@@ -80,10 +121,12 @@ public class WaypointMovement : MonoBehaviour
 
     void SetTargetWaypoint(Transform waypoint)
     {
+        ResetLerpValues();        
         targetWaypoint = waypoint;
         currentWaypoint = null;
+        SetLerpValues(homeWaypoint.position, targetWaypoint.position);
         IsMoving = true;
-        onTargetLeave.Invoke();
+        onTargetLeave.Invoke();       
     }
 
     //Returns a random interger within a range with the expection of the provided int
