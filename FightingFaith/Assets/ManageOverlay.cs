f265f1e65597a50c689e42ecf7d6a46170712219
arrozm <57187091+arrozm@users.noreply.github.com>
2020-04-03 05:48:59 -0400

    Add Player Damage UI
    Screen pulses red after player is hit. Heartbeat sound effect added.

---
+++ FightingFaith/Assets/ManageOverlay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageOverlay : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] overlay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<Health>().CurrentHealth == 2)
+        {
+            overlay[0].SetActive(true);
+            overlay[1].SetActive(false);
+        } else if (player.GetComponent<Health>().CurrentHealth == 1)
+        {
+            overlay[0].SetActive(false);
+            overlay[1].SetActive(true);
+        }
+    }
+}
