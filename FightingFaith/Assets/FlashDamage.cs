bdab1faa42cad121388d8c9a2e797e8968694dc4
moradavi <57117336+moradavi@users.noreply.github.com>
2020-04-02 22:54:05 -0400

    Adjust Enemy Flash
    Enemy flash now covers entire enemy sprite. Flash Damage script now changes material of sprites to be all white.

--- FightingFaith/Assets/FlashDamage.cs
+++ FightingFaith/Assets/FlashDamage.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class FlashDamage : MonoBehaviour
 {
-    Animator anim;
+    public List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+    public Material defaultMaterial;
+    public Material flashMaterial;
+    bool isFlashing;
+    public float flashTime;
+    float timer;
 
     private void Start()
     {
-        anim = GetComponent<Animator>();
+        isFlashing = false;
+    }
+
+    private void Update()
+    {
+        if(isFlashing == true)
+        {
+            timer += Time.deltaTime;
+            if(timer > flashTime)
+            {
+                foreach (SpriteRenderer sp in spriteRenderers)
+                {
+                    sp.material = defaultMaterial;
+                }
+                isFlashing = false;
+                timer = 0;
+            }
+        }
     }
 
     public void FlashTrigger()
     {
-        anim.SetTrigger("hit");
+
+        isFlashing = true;
+        foreach(SpriteRenderer sp in spriteRenderers)
+        {
+            sp.material = flashMaterial; 
+        }
     }
 }
