19040e6c7ad0be69a87618a9db42160d90e17585
arrozm <57187091+arrozm@users.noreply.github.com>
2020-04-03 10:07:50 -0400

    Adjust Trail Renderer
    Trail renderer now has a particel effect and smaller swipe length.

---
+++ FightingFaith/Assets/Scripts/TrailRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailRenderer : MonoBehaviour
+{
+    Vector3 lastMousePosition;
+    public float swipeSpeedThreshold;
+    private ParticleSystem ps;
+
+    public GameObject particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetMouseButton(0))
+        //{
+        //    Debug.Log("Play");
+        //    ps.enableEmission = true;
+        //} else
+        //{
+        //    ps.enableEmission = false;
+        //}
+
+        if (Input.GetMouseButton(0))
+        {
+            particleSystem.SetActive(true);
+        } else
+        {
+            particleSystem.SetActive(false);
+        }
+    }
+}
